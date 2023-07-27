26463924313fe4d7ec12862653b61f7eca86b4b6
mccauslander <mccauslander@hotmail.com>
2023-07-27 16:29:49 +1000

    Prep for homework

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+
+    private bool mainMenuActive = true;
+    private bool optionsMenuActive = false;
+    private bool quitPromptActive = false;
+
+    private Vector2 buttonSize;
+    private int fontSize = 80;
+    private Vector2 promptSize;
+    private Vector2 promptPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
